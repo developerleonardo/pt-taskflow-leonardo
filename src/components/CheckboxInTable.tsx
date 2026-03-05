a4b5274c22da3c60f80eec79e650bb3cdcca930b
developerleonardo <leo.11hnz@gmail.com>
2026-03-05 17:26:03 -0500

    feat: move tableData to a separate mock file for better organization

--- src/components/CheckboxInTable.tsx
+++ src/components/CheckboxInTable.tsx
@@ -3,69 +3,7 @@ import { ToDoItem } from "@/components/ToDoItem";
 import { useState } from "react";
 import { Button } from "./ui/button";
 import { Plus } from "lucide-react";
-
-const tableData = [
-  {
-    id: "1",
-    todo: "Prototype app",
-    completed: true,
-    userid: "26",
-  },
-  {
-    id: "2",
-    todo: "Implement user authentication",
-    completed: false,
-    userid: "42",
-  },
-  {
-    id: "3",
-    todo: "Design database schema",
-    completed: true,
-    userid: "17",
-  },
-  {
-    id: "4",
-    todo: "Set up project repository",
-    completed: true,
-    userid: "8",
-  },
-  {
-    id: "5",
-    todo: "Create API endpoints",
-    completed: false,
-    userid: "13",
-  },
-  {
-    id: "6",
-    todo: "Build UI components",
-    completed: false,
-    userid: "34",
-  },
-  {
-    id: "7",
-    todo: "Write unit tests",
-    completed: true,
-    userid: "21",
-  },
-  {
-    id: "8",
-    todo: "Integrate payment gateway",
-    completed: false,
-    userid: "19",
-  },
-  {
-    id: "9",
-    todo: "Optimize performance",
-    completed: true,
-    userid: "5",
-  },
-  {
-    id: "10",
-    todo: "Deploy to production",
-    completed: false,
-    userid: "11",
-  },
-];
+import { tableData } from "@/mocks/tasks.mocks";
 
 export const CheckboxInTable = () => {
   const [todos, setTodos] = useState(tableData);

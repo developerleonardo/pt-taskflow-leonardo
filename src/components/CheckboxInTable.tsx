cc959bbc3c2518ef0cce29c088501b0e7b0572f9
developerleonardo <leo.11hnz@gmail.com>
2026-03-06 08:06:57 -0500

    feat: enhance scrollbar styling and improve overflow handling in CheckboxInTable

--- src/components/CheckboxInTable.tsx
+++ src/components/CheckboxInTable.tsx
@@ -30,16 +30,16 @@ export const CheckboxInTable = () => {
   const pendingTasks = filteredTodos.filter((item) => item.completed === false);
   return (
     <>
-      <section className="flex flex-col h-1/2 min-h-0 overflow-y-auto">
-        <h2 className=" bg-red-100 rounded-lg px-2 py-1 mb-4 text-lg font-medium">
+      <section className="flex flex-col h-1/2 min-h-0">
+        <h2 className="bg-red-100 rounded-lg px-2 py-1 mb-4 text-lg font-medium">
           To Do
         </h2>
-        <div className="flex flex-col gap-2">
-          <div className="flex justify-between pl-10 pr-12 py-1 border-b border-b-neutral-300">
+        <div className="flex flex-col flex-1 min-h-0 gap-2">
+          <div className="flex justify-between pl-10 pr-12 py-1 border-b border-neutral-300">
             <span className="text-sm text-muted-foreground">Description</span>
             <span className="text-sm text-muted-foreground">User ID</span>
           </div>
-          <div className="flex flex-col gap-2 flex-1 max-h-63">
+          <div className="flex flex-col gap-2 flex-1 overflow-y-auto">
             {pendingTasks.map((item) => (
               <ToDoItem
                 key={item.id}
@@ -52,6 +52,7 @@ export const CheckboxInTable = () => {
             ))}
           </div>
         </div>
+
         <Button
           className="mt-4 place-self-end"
           size="sm"
@@ -61,16 +62,16 @@ export const CheckboxInTable = () => {
           Add Task
         </Button>
       </section>
-      <section className="flex flex-col h-1/2 min-h-0 overflow-y-auto">
+      <section className="flex flex-col h-1/2 min-h-0">
         <h2 className="bg-green-100 rounded-lg px-2 py-1 mb-4 text-lg font-medium">
           Completed
         </h2>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col flex-1 min-h-0 gap-2">
           <div className="flex justify-between pl-10 pr-12 py-1 border-b border-b-neutral-300">
             <span className="text-sm text-muted-foreground">Description</span>
             <span className="text-sm text-muted-foreground">User ID</span>
           </div>
-          <div className="flex flex-col gap-2 flex-1 max-h-63">
+          <div className="flex flex-col gap-2 flex-1 overflow-y-auto">
             {completedTasks.map((item) => (
               <ToDoItem
                 key={item.id}
